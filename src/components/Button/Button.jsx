d83b417dc4138674cac253e41af9f8abf6e3f56a
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-11-17 18:29:02 +0100

    button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import './Button.css';
 
 const Button = ({ onClick, disabled }) => (
-  <button type="button" className="Button" onClick={onClick} disabled={disabled}>
-    Load more
-  </button>
+  <div className='Button-item'>
+    <button type="button" className="Button" onClick={onClick} disabled={disabled}>
+      Load more
+    </button>
+  </div>
 );
 
 Button.propTypes = {
