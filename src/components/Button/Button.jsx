6151d19560b6a167d3e87695e82d4994c795b80f
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-11-18 20:06:10 +0100

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-const Button = ({ onClick, }) => (
+const Button = ({ onClick, disable }) => (
 
-  <button type="button" className={styles.Button} onClick={onClick} >
+  <button type="button" className={styles.Button} onClick={onClick} disabled={disable}>
     Load more
   </button>
 );
